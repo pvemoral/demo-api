a7adb5374d4aa8e8ccf8977f5705caddc51cbbad
joan <joan@joans-MacBook-Pro.local>
2019-02-20 09:23:16 +0100

    Removing lombok from entity service

--- src/main/java/com/api/demo/services/EntityServiceImpl.java
+++ src/main/java/com/api/demo/services/EntityServiceImpl.java
@@ -3,20 +3,19 @@ package com.api.demo.services;
 import com.api.demo.models.ModelEntity;
 import com.api.demo.models.ModelEntityDTO;
 import com.api.demo.repositories.EntityPersistence;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.api.demo.repositories.ModelEntityRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class EntityServiceImpl implements EntityService {
 
     private final EntityPersistence repository;
+
+    public EntityServiceImpl(EntityPersistence repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<ModelEntity> findAll() {
